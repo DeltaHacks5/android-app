d84091e1bc5b214096033108ef4990007c036cbd
dquach217 <dq6180@rit.edu>
2019-01-27 03:29:24 -0500

    add get api request

--- app/src/main/java/me/susieson/sportscanner/ExploreFragment.java
+++ app/src/main/java/me/susieson/sportscanner/ExploreFragment.java
@@ -7,10 +7,17 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -19,6 +26,12 @@ import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class ExploreFragment extends Fragment implements OnMapReadyCallback {
 
@@ -50,6 +63,44 @@ public class ExploreFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+//        ArrayList<String> Addresses = new ArrayList<>();
+//        ArrayList<String> Ratings = new ArrayList<>();
+//        ArrayList<String> Names = new ArrayList<>();
+//        ArrayList<String> IDs = new ArrayList<>();
+//
+//        RequestQueue requestQueue = (RequestQueue) Volley.newRequestQueue(this);
+//
+//        String JsonURL = "http://sportscanner.net:8080/";
+//
+//        JsonArrayRequest obreq = new JsonArrayRequest(Request.Method.GET, JsonURL, null,
+//                new Response.Listener<JSONArray>() {
+//                    @Override
+//                    public void onResponse(JSONArray response) {
+//                        try {
+//                            for(int i=0; i < response.length(); i++){
+//                                JSONObject obj = response.getJSONObject(i);
+//                                Names.add(obj.getString("placeName"));
+//                                Addresses.add(obj.getString("placeAddress"));
+//                                Ratings.add(obj.getString("placeRating"));
+//                                IDs.add(obj.getString("_id"));
+//                                String placeLatitude = obj.getString("placeLatitude");
+//                                String placeLongitude = obj.getString("placeLongitude");
+//                                mMap.addMarker(new MarkerOptions().position(new LatLng(Double.parseDouble(placeLatitude),Double.parseDouble(placeLongitude))).title(Names.get(Names.size() - 1)).snippet(Addresses.get(Addresses.size() - 1)));
+//                            }
+//                        }
+//                        catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                },
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Log.e("Volley", error.toString());
+//                    }
+//                }
+//
+//        );
 
         mMap.addMarker(new MarkerOptions()
                 .position(hamilton)
